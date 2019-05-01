9e120a83883512528e0a665f8c5bdfe008f8895c
khaiz18 <khaizlett@gmail.com>
2019-05-01 21:28:01 +0200

    created person

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 
 class App extends Component {
 render(){
-/*return (
+return (
 
   <div className="App">
     <h1> Hi, I'm a React App</h1>
+    <p>This is really working!</p>
   </div>
 
 
-      );*/
-    return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work now?'))
+      );
+ //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Does this work now?'))
     }
 
   }
