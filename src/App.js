772b8b2899a83eabe42a770b86d98f1d6e2627de
khaiz18 <khaizlett@gmail.com>
2019-05-02 21:03:50 +0200

    handling events with methods

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Person from './Person/Person';
 
 
 class App extends Component {
-render(){
+ 
+  state = {
+
+       persons: [
+        {name:"Leo", age: "21" },
+        {name:"clark", age:"43"},
+        {name:"Jenny", age:"26"}
+
+       ]
+
+  }
+
+  render(){
+
 return (
 
   <div className="App">
     <h1> Hi, I'm a React App</h1>
     <p>This is really working!</p>
-    <Person name="Keith" age="29" />
-    <Person name="James" age="22">I love money!</Person>
-    <Person name="Kent" age="28" />
+    <button>Switch name</button>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+    <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>I love money!</Person>
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
   </div>
 
 
