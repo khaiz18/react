a230015651953500472bce805aa6086ad82890b1
khaiz18 <khaizlett@gmail.com>
2019-05-02 20:12:57 +0200

    math.floor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ return (
   <div className="App">
     <h1> Hi, I'm a React App</h1>
     <p>This is really working!</p>
+    <Person/>
   </div>
 
 
