432d7ef69301a25747aefde7158f2b2c7cff6d64
khaiz18 <khaizlett@gmail.com>
2019-05-02 21:58:06 +0200

    useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ class App extends Component {
 
   }
 
+
+  switchNameHandler = () => {
+
+      console.log("clicked");
+
+
+  }
+
   render(){
 
 return (
@@ -23,7 +31,7 @@ return (
   <div className="App">
     <h1> Hi, I'm a React App</h1>
     <p>This is really working!</p>
-    <button>Switch name</button>
+    <button onClick= {this.switchNameHandler}> Switch name</button>
     <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
     <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>I love money!</Person>
     <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
